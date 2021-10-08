fc44af39a3b4635f0b203499b855f505ff89c1b9
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-08 19:41:30 +0000

    to fix test

--- src/test/java/io/turntabl/student/NaughtyStudentTest.java
+++ src/test/java/io/turntabl/student/NaughtyStudentTest.java
@@ -3,19 +3,13 @@ package io.turntabl.student;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NaughtyStudentTest {
-    List<Double> grades;
-
-    @BeforeEach
-    void setUp() {
-        grades = List.of(20.00, 30.00, 40.00, 10.00);
-    }
 
     @Test
     void testThatAverageGradeIsHigherBy10Percent() {
+        List<Double> grades = List.of(20.00, 30.00, 40.00, 10.00);
         Student student = new Student(grades);
 
         double percentIncreasee = 0.1;
@@ -25,4 +19,19 @@ public class NaughtyStudentTest {
         Assertions.assertNotEquals(expected, actual);
         Assertions.assertTrue(expected > actual);
     }
+
+    // @Test
+    // void testThatScoresAreHigherThanTheyOughtToBe() {
+    //     Lecture lecture = new Lecture();
+    //     lecture.enter(new NaughtyStudent(List.of(15.00, 20.00, 25.00)));
+    //     lecture.enter(new NaughtyStudent(List.of(5.00, 5.00, 20.00)));
+
+    //     List<Double> averageGrades = lecture.getStudentList().stream().map(student -> student.getAverageGrade()).toList();
+
+    //     double expected = 20.00;
+    //     Assertions.assertTrue(expected < averageGrades.get(0));
+
+    //     expected = 10.0;
+    //     Assertions.assertTrue(expected < averageGrades.get(1));
+    // }
 }
