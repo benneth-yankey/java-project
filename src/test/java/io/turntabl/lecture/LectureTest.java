4b08fabc62ad35971b6812096b3d09b754ffa50c
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-09 18:23:59 +0000

    added test for naughty students score higher than normal

--- src/test/java/io/turntabl/lecture/LectureTest.java
+++ src/test/java/io/turntabl/lecture/LectureTest.java
@@ -1,13 +1,12 @@
 package io.turntabl.lecture;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.turntabl.student.NaughtyStudent;
 import io.turntabl.student.Student;
 
 public class LectureTest {
@@ -20,12 +19,23 @@ public class LectureTest {
 
     @Test
     void testGetHighestAverageGrade() {
-       lecture.enter(new Student(List.of(20.00, 30.00))); 
-       lecture.enter(new Student(List.of(40.00, 60.00))); 
+        Lecture lecture = new Lecture();
+        lecture.enter(new Student(List.of(20.00, 30.00)));
+        lecture.enter(new Student(List.of(40.00, 60.00)));
 
-       double actual = lecture.getHighestAverageGrade();
+        double actual = lecture.getHighestAverageGrade();
 
-       assertEquals(50.00, actual);
-       assertNotEquals(25.00, actual);
+        Assertions.assertEquals(50.00, actual);
+        Assertions.assertNotEquals(25.00, actual);
+    }
+
+    @Test
+    void testThatNaughtyStudentScoreHightThanOughtToBe() {
+        lecture.enter(new NaughtyStudent(List.of(15.00, 20.00, 25.00)));
+        lecture.enter(new NaughtyStudent(List.of(20.00, 50.00, 50.00)));
+       
+        double expected = 44.00; 
+        Assertions.assertTrue(expected > lecture.getHighestAverageGrade());
+        Assertions.assertNotEquals(expected, lecture.getHighestAverageGrade());
     }
 }
