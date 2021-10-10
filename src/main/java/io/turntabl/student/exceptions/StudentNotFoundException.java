a6c9660ab3973fee17933c6bdd26c213658b9b75
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-10 17:18:49 +0000

    adds student not found exception

---
+++ src/main/java/io/turntabl/student/exceptions/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package io.turntabl.student.exceptions;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
