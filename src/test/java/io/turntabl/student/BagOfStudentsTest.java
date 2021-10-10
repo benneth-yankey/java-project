21b9ee337d47c4c16fcddc2a191166c888c6d3b0
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-10 00:03:07 +0000

    refactor test

--- src/test/java/io/turntabl/student/BagOfStudentsTest.java
+++ src/test/java/io/turntabl/student/BagOfStudentsTest.java
@@ -3,25 +3,33 @@ package io.turntabl.student;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.turntabl.enums.Level;
 
 public class BagOfStudentsTest {
-    @Test
-    void testThatStudentsCanBeAddedToTheBag() {
+    BagOfStudents<Student> bag;
+    Student student1;
+    Student student2;
+
+    @BeforeEach
+    void setUp() {
         // given
-        Student student1 = new Student(List.of(20.00, 30.00), Level.ONE);
-        Student student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
+        student1 = new Student(List.of(20.00, 30.00), Level.ONE);
+        student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
 
         // when
-        BagOfStudents<Student> bag = new BagOfStudents<>();
+        bag = new BagOfStudents<>();
         bag.add(student1);
         bag.add(student1);
         bag.add(student2);
         bag.add(student2);
         bag.add(student2);
+    }
 
+    @Test
+    void testThatStudentsCanBeAddedToTheBag() {
         // then
         Assertions.assertEquals(2, bag.getBagCount());
         Assertions.assertEquals(2, bag.getCount(student1));
@@ -29,19 +37,8 @@ public class BagOfStudentsTest {
     }
 
     @Test
-    void testClear() {
-        // given
-        Student student1 = new Student(List.of(20.00, 30.00), Level.ONE);
-        Student student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
-
+    void testThatBagIsEmpty() {
         // when
-        BagOfStudents<Student> bag = new BagOfStudents<>();
-        bag.add(student1);
-        bag.add(student1);
-        bag.add(student2);
-        bag.add(student2);
-        bag.add(student2);
-
         bag.clear();
 
         // then
@@ -50,30 +47,27 @@ public class BagOfStudentsTest {
     }
 
     @Test
-    void testRemove() {
-        // given
-        Student student1 = new Student(List.of(20.00, 30.00), Level.ONE);
-        Student student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
-
+    void testThatMultipleStudentCountDecreasesWhenRemoved() {
         // when
-        BagOfStudents<Student> bag = new BagOfStudents<>();
-        bag.add(student1);
-        bag.add(student1);
-        bag.add(student2);
-        bag.add(student2);
-        bag.add(student2);
-
         bag.remove(student1);
         bag.remove(student2);
 
         // then
         Assertions.assertEquals(1, bag.getCount(student1));
         Assertions.assertEquals(2, bag.getCount(student2));
+    }
 
+    @Test
+    void testThatStudentIsRemovedWhenCountIsZero() {
         // when
         bag.remove(student1);
-        
+        bag.remove(student1);
+        bag.remove(student2);
+        bag.remove(student2);
+        bag.remove(student2);
+
         // then
         Assertions.assertEquals(0, bag.getCount(student1));
+        Assertions.assertEquals(0, bag.getCount(student2));
     }
 }
