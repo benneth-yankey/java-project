4c10e73d89096ec6a2364f0c428bf8b2ab161611
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-10 16:21:08 +0000

    refactor tests

--- src/test/java/io/turntabl/student/BagOfStudentsTest.java
+++ src/test/java/io/turntabl/student/BagOfStudentsTest.java
@@ -3,71 +3,58 @@ package io.turntabl.student;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.turntabl.enums.Level;
 
 public class BagOfStudentsTest {
-    BagOfStudents<Student> bag;
-    Student student1;
-    Student student2;
+    BagOfStudents<Student> bag = new BagOfStudents<>();
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void testClear_GivenThatBagIsNotEmpty_ThenReturnEmptyBag() {
         // given
-        student1 = new Student(List.of(20.00, 30.00), Level.ONE);
-        student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
+        Student student1 = new Student(List.of(20.00, 30.00), Level.ONE);
+        NaughtyStudent student2 = new NaughtyStudent(List.of(15.00, 22.00), Level.TWO);
 
         // when
-        bag = new BagOfStudents<>();
         bag.add(student1);
         bag.add(student1);
         bag.add(student2);
-        bag.add(student2);
-        bag.add(student2);
-    }
 
-    @Test
-    void testThatStudentsCanBeAddedToTheBag() {
-        // then
-        Assertions.assertEquals(2, bag.getBagCount());
-        Assertions.assertEquals(2, bag.getCount(student1));
-        Assertions.assertEquals(3, bag.getCount(student2));
-    }
-
-    @Test
-    void testThatBagIsEmpty() {
-        // when
         bag.clear();
 
         // then
         Assertions.assertEquals(0, bag.getBagCount());
-        Assertions.assertEquals(0, bag.getCount(student1));
     }
 
     @Test
-    void testThatMultipleStudentCountDecreasesWhenRemoved() {
+    void testRemove_GivenThatAStudentCountIsMoreThanOne_ThenDecreaseTheStudentCount() {
+        // given
+        Student student = new Student(List.of(56.45, 78.30, 66.50), Level.ONE);
+
         // when
-        bag.remove(student1);
-        bag.remove(student2);
+        bag.add(student);
+        bag.add(student);
+
+        bag.remove(student);
 
         // then
-        Assertions.assertEquals(1, bag.getCount(student1));
-        Assertions.assertEquals(2, bag.getCount(student2));
+        Assertions.assertEquals(1, bag.getCount(student));
     }
 
     @Test
-    void testThatStudentIsRemovedWhenCountIsZero() {
+    void testRemove_GivenThatStudentCountIsEqualToOne_ThenRemoveStudent() {
+        // given
+        NaughtyStudent naughtstudent = new NaughtyStudent(List.of(35.55, 22.45, 55.50), Level.ONE);
+
         // when
-        bag.remove(student1);
-        bag.remove(student1);
-        bag.remove(student2);
-        bag.remove(student2);
-        bag.remove(student2);
+        bag.add(naughtstudent);
+        bag.add(naughtstudent);
+
+        bag.remove(naughtstudent);
+        bag.remove(naughtstudent);
 
         // then
-        Assertions.assertEquals(0, bag.getCount(student1));
-        Assertions.assertEquals(0, bag.getCount(student2));
+        Assertions.assertEquals(0, bag.getCount(naughtstudent));
     }
 }
--- src/test/java/io/turntabl/student/StudentTest.java
+++ src/test/java/io/turntabl/student/StudentTest.java
@@ -3,32 +3,21 @@ package io.turntabl.student;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.turntabl.enums.Level;
 
 public class StudentTest {
-    List<Double> grades;
-
-    @BeforeEach
-    void setUp() {
-        grades = List.of(20.00, 30.00, 40.00, 10.00);
-    }
 
     @Test
-    void testThatStudentIsCreated() {
-        Student student = new Student(grades, Level.ONE);
-        Assertions.assertNotNull(student);
-    }
+    void testGetAverageGrade_GivenStudentWithGrades_ThenReturnAverageGrade() {
+        // given
+        List<Double> grades = List.of(20.00, 30.00, 40.00, 10.00);
 
-    @Test
-    void testGetAverageGrade() {
+        // when
         Student student = new Student(grades, Level.TWO);
 
-        double expected = 100.00 / grades.size();
-        double actual = student.getAverageGrade();
-
-        Assertions.assertEquals(expected, actual);
+        // then
+        Assertions.assertEquals((100 / 4), student.getAverageGrade());
     }
 }
