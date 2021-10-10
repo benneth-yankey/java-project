b507573a9a5fe8314ae8d021bfcd7daa283b206b
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-10 17:19:02 +0000

    adds get student method

--- src/main/java/io/turntabl/register/Register.java
+++ src/main/java/io/turntabl/register/Register.java
@@ -8,46 +8,50 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.TreeMap;
 
 import io.turntabl.enums.Level;
 import io.turntabl.student.Student;
+import io.turntabl.student.exceptions.StudentNotFoundException;
 
 public class Register {
-    // stores list of nameables
     private List<Student> students = new ArrayList<>();
 
     public Register(List<Student> students) {
         this.students = students;
     }
 
-    // returns list of nameables (students)
     public List<Student> getStudents() {
         return students;
     }
 
-    // returns names of nameables
     public List<String> getRegister() {
         return students.stream().map(Student::getName).toList();
     }
 
-    // returns names of nameables by level
     public Map<Level, List<Student>> getRegisterByLevel(Level level) {
         return students.stream().filter(student -> student.getLevel().equals(level))
                 .collect(groupingBy(Student::getLevel));
     }
 
-    // returns names of nameables grouped by their level
     public String printReport() {
         Map<Level, List<String>> map = students.stream()
                 .collect(groupingBy(Student::getLevel, mapping(Student::getName, toList())));
 
-        // convert a Map to a TreeMap to sort keys
         TreeMap<Level, List<String>> s = new TreeMap<>(map);
         return s.toString();
     }
 
     public List<Student> sort(Comparator<Student> comparator) {
         return students.stream().sorted(comparator).toList();
+    }
+
+    public Student getStudent(String name) throws StudentNotFoundException {
+       Optional<Student> s = students.stream().filter(student -> student.getName().equals(name)).findFirst();
+       if (s.isEmpty()) {
+           throw new StudentNotFoundException("student does not exist");
+       }
+       return s.get();
     }
 }
