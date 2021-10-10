f67cf63ad5467454753ecba8094234d0ac4c2081
benneth-yankey <benneth.yankey@turntabl.io>
2021-10-10 17:18:22 +0000

    adds get student by name tests

--- src/test/java/io/turntabl/register/RegisterTest.java
+++ src/test/java/io/turntabl/register/RegisterTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import io.turntabl.enums.Level;
 import io.turntabl.student.NaughtyStudent;
 import io.turntabl.student.Student;
+import io.turntabl.student.exceptions.StudentNotFoundException;
 
 public class RegisterTest {
 
@@ -113,6 +114,45 @@ public class RegisterTest {
 
         // then
         List<Student> expected = List.of(student1, student2, naughtyStudent2, naughtyStudent1);
-        Assertions.assertEquals(expected, register.sort((s1, s2) -> Double.compare(s2.getAverageGrade(), s1.getAverageGrade())));
+        Assertions.assertEquals(expected,
+                register.sort((s1, s2) -> Double.compare(s2.getAverageGrade(), s1.getAverageGrade())));
+    }
+
+    @Test
+    void testGetStudent_GivenStudentName_ThenReturnTheStudent() throws StudentNotFoundException {
+        // given
+        Student student1 = new Student(List.of(65.50, 77.22, 80.00), Level.ONE);
+        Student student2 = new Student(List.of(95.00, 56.50, 75.35), Level.THREE);
+
+        student1.setName("dominic");
+        student2.setName("rebecca");
+    
+        // when
+        List<Student> students = List.of(student1, student2);
+        Register register = new Register(students);
+
+        // then
+        Assertions.assertEquals(student1, register.getStudent("dominic"));
+    }
+
+    @Test
+    void testGetStudent_GivenStudentNameThatDoesNotExist_ItThrowsStudentNotFoundException() {
+        // given
+        Student student1 = new Student(List.of(50.55, 68.33, 45.89), Level.ONE);
+        Student student2 = new Student(List.of(88.23, 35.65, 67.80), Level.THREE);
+
+        student1.setName("dominic");
+        student2.setName("rebecca");
+    
+        // when
+        List<Student> students = List.of(student1, student2);
+        Register register = new Register(students);
+
+        Exception exception = Assertions.assertThrows(StudentNotFoundException.class, () -> {
+           register.getStudent("benneth");
+        });
+
+        // then
+        Assertions.assertEquals("student does not exist", exception.getMessage());
     }
 }
